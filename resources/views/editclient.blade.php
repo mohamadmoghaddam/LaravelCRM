0bb5e11fe4dd5fad8f471cf8036be606bc411860
mohamad <mohamadreza.ghasemi1383@gmail.com>
2023-09-11 21:58:26 +0430

    fix editclient view

--- resources/views/editclient.blade.php
+++ resources/views/editclient.blade.php
@@ -21,7 +21,7 @@
                     <!-- Add Client -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Create Client</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Edit Client</h6>
                         </div>
                         <div class="card-body">
                             <div class="p-5">
